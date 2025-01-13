db428108c24f727f6df40a6a24628159fab01186
web-space-pro <ludmilamedvedeva@MacBook-Pro-Ludmila.local>
2025-01-14 00:13:12 +0200

    services

---
+++ parts/tpl-services.php
@@ -0,0 +1,63 @@
+<?php
+if ( function_exists('get_field') ) {
+    $title  = get_sub_field('title');
+    $count = -1;
+    $count_post  = get_sub_field('count_post');
+    if(!empty($count_post)){
+        $count = $count_post;
+    }
+}
+
+
+?>
+<?php
+// Получаем записи всех участников ( кроме текущего )
+$args = array(
+    'post_type' => 'services',
+    'posts_per_page' => $count,
+    'orderby' => 'date',
+    'order' => 'ASC',
+);
+
+$specialist_loop = new WP_Query($args);
+?>
+<section id="services" class="tplServices">
+    <div class="container">
+        <div class="tplServices__heading">
+            <?php if(!empty($title)) : ?>
+                <h2 class="title"><?=$title;?></h2>
+            <?php endif; ?>
+        </div>
+        <?php if ($specialist_loop->have_posts()) : ?>
+        <div class="tplServices__loop">
+            <?php while ($specialist_loop->have_posts()) : $specialist_loop->the_post(); ?>
+                <figure class="services__card">
+                    <a href="<?=get_permalink()?>" target="_self">
+                        <div class="relative">
+                            <div class="services__photo">
+                                <?php med_plus_post_thumbnail();?>
+                            </div>
+                            <?php if( have_rows('services') ): ?>
+                                <?php while( have_rows('services') ): the_row(); ?>
+                                    <?php
+                                    if( get_row_layout() == 'services_welcome' ): ?>
+                                        <div class="specialist__experience"><?php echo the_sub_field('price'); ?> ₽</div>
+                                    <?php endif;
+                                endwhile; ?>
+                            <?php endif; ?>
+
+                        </div>
+                        <div class="services__title">
+                            <h3> <?=get_the_title()?></h3>
+                        </div>
+                    </a>
+                </figure>
+            <?php endwhile; else:
+                echo '<p>Других услуг в этой категории пока нет.</p>';
+            endif;
+            wp_reset_postdata();
+            ?>
+        </div>
+    </div>
+</section>
+
